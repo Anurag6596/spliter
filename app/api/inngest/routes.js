11341e0c4aeefdbe97b44dd9418039a19c735264
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2025-09-16 22:34:40 +0530

    fixed soe of the bugs found nad integrated the Gemini API for the mials

--- app/api/inngest/routes.js
+++
@@ -1,9 +0,0 @@
-// app/api/inngest/route.js
-import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest/client";
-import { paymentReminders } from "@/lib/inngest/payment-reminder";
-
-export const { GET, POST } = serve({
-  client: inngest,
-  functions: [paymentReminders],
-});
